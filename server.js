4eab1694ed5d13bab34d3eebe3a25f3a8eedc7d1
Jordan Chadsey <jordan@identipop.com>
2017-12-31 21:17:25 -0500

    changes

--- server.js
+++ server.js
@@ -4,29 +4,115 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
-var exphbs = require("exphbs");
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var exphbs = require("express-handlebars");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mogulNotes";
 
-// Require all models
+
 var db = require("./models");
 
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
-// Initialize Express
+
 var app = express();
 
-// Configure middleware
 
-// Use morgan logger for logging requests
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set ("view engine", "handlebars");
+
 app.use(logger("dev"));
-// Use body-parser for handling form submissions
+
 app.use(bodyParser.urlencoded({ extended: false }));
-// Use express.static to serve the public folder as a static directory
+
 app.use(express.static("public"));
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
+
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI,{
   useMongoClient: true
+});
+
+app.get ("/", function (req, res){
+  res.render("index");
+});
+app.get("/scrape", function(req, res) {
+
+  axios.get("https://smallbiztrends.com/").then(function(response) {
+
+    var $ = cheerio.load(response.data);
+
+
+    $("h2").each(function(i, element) {
+
+      var result = {};
+
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+
+      db.Article
+        .create(result)
+        .then(function(dbArticle) {
+
+          res.send("Scrape Complete");
+        })
+        .catch(err => console.log(err));
+    });
+  });
+});
+
+
+app.get("/articles", function(req, res) {
+
+  db.Article
+    .find({})
+    .then(function(dbArticle) {
+
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+
+      res.json(err);
+    });
+});
+
+
+app.get("/articles/:id", function(req, res) {
+
+  db.Article
+    .findOne({ _id: req.params.id })
+
+    .populate("note")
+    .then(function(dbArticle) {
+
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+
+  db.Note
+    .create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+
+      res.json(err);
+    });
+});
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
